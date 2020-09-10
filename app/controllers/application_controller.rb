d530333a8e8d638b7d9233fb381aa28bf8c0bcd8
Shota1104 <70424405+Shota1104@users.noreply.github.com>
2020-09-10 18:29:15 +0900

    ログイン分岐

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,7 @@
 class ApplicationController < ActionController::Base
   before_action :basic_auth
-
+  before_action :configure_permitted_parameters, if: :devise_controller?
+  
   private
 
   def basic_auth
@@ -9,4 +10,7 @@ class ApplicationController < ActionController::Base
     end
   end
 
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname, :last_name, :first_name, :kana_last_name, :kana_first_name, :birthday])
+  end
 end
