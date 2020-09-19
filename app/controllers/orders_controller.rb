a464296e5d0b19aef5eec7175e385479b3d26818
Shota1104 <70424405+Shota1104@users.noreply.github.com>
2020-09-19 17:27:56 +0900

    paramsを持たせ購入者画面へ遷移

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,9 +1,15 @@
 class OrdersController < ApplicationController
 
   def index
+    @item = Item.find(params[:item_id])
   end
 
   def create
   end
+
+  # private
+  # def item_params
+  #   params.require(:item).permit(:image, :name, :explanation, :category_id, :status_id, :delivery_cost_id, :delivery_origin_id, :delivery_day_id, :price).merge(user_id: current_user.id, item_id: params[:item_id])
+  # end
 
 end
