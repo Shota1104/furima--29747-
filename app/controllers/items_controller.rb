f121d996ad434adb9e1046dc2ab51deaa6143af0
Shota1104 <70424405+Shota1104@users.noreply.github.com>
2020-09-23 16:21:38 +0900

    未ログインでも詳細ページ遷移

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,5 @@
 class ItemsController < ApplicationController
-before_action :move_to_sign_in, except: [:index]
+before_action :move_to_sign_in, except: [:index, :show]
 
 before_action :set_item, only: [:edit, :show, :update, :destroy]
 
